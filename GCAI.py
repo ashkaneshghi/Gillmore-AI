5cc2c4585b867a501ea27f2aaef6ae939f682681
ashkaneshghi <116697979+ashkaneshghi@users.noreply.github.com>
2023-04-03 08:27:45 +0100

    Update GCAI.py

--- GCAI.py
+++ GCAI.py
@@ -96,7 +96,7 @@ def res(prompt,AIAPI):
         graph_configs=[graph_config])
 
     memory = ConversationBufferMemory(memory_key="chat_history")
-    llm=OpenAI(temperature=0.2, openai_api_key=AIAPI)
+    llm=OpenAI(temperature=0.1, openai_api_key=AIAPI)
     agent_chain = create_llama_chat_agent(
         toolkit,
         llm,

5ebb212679d30695ae186dc677946eb0a8153998
ashkaneshghi <116697979+ashkaneshghi@users.noreply.github.com>
2023-04-03 11:32:57 +0100

    Update GCAI.py

--- GCAI.py
+++ GCAI.py
@@ -16,10 +16,6 @@ from llama_index import GPTSimpleVectorIndex, download_loader
 from pathlib import Path
 import openai
 
-# os.environ['OPENAI_API_KEY'] = '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' 
-# os.environ['OPENAI_API_KEY'] = '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'
-
-# Set Streamlit page configuration
 st.set_page_config(page_title='Gillmore AI', layout='wide')
 # Initialize session states
 if "generated" not in st.session_state:
@@ -31,23 +27,26 @@ if "input" not in st.session_state:
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
 
+# def get_text():
+#     input_text = st.text_input("You: ", st.session_state["input"], key="input",
+#                             placeholder="Please type your question here ...", 
+#                             label_visibility='hidden')
+#     return input_text
 
-# Define function to get user input
 def get_text():
-    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+    input_text = st.text_input("You: ", "", key="input",
                             placeholder="Please type your question here ...", 
                             label_visibility='hidden')
     return input_text
 
-# Define function to start a new chat
 def new_chat():
     save = []
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         save.append("User:" + st.session_state["past"][i])
         save.append("Bot:" + st.session_state["generated"][i])        
     st.session_state["stored_session"].append(save)
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
+#     st.session_state["generated"] = []
+#     st.session_state["past"] = []
     st.session_state["input"] = ""
     # st.session_state.entity_memory.store = {}
     # st.session_state.entity_memory.buffer.clear()
@@ -65,12 +64,11 @@ def new_chat():
 #     #MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
 #     K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
 
-# Set up the Streamlit app layout
+
 st.title(" Gillmore AI ")
 # st.subheader(" Presented by Gillmore Centre for Financial Technology")
 # st.subheader(" Powered by Llama_Index + LangChain + OpenAI + Streamlit")
 
-# Ask the user to enter their OpenAI API key
 API_O = st.sidebar.text_input("API-KEY", type="password")
 
 # Session state storage would be ideal
@@ -185,7 +183,6 @@ def showres(prompt):
 
 user_input = get_text()
 
-# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
 if user_input:
     showres(user_input)
     st.button("Send", on_click = showres(user_input), type='primary')
@@ -204,10 +201,7 @@ if user_input:
 #     st.session_state.generated.append(output)  
 
     
-
-# Allow to download as well
 download_str = []
-# Display the conversation history using an expander, and allow the user to download it
 with st.expander("Conversation", expanded=True):
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         st.info(st.session_state["past"][i],icon="🧐")
@@ -215,21 +209,18 @@ with st.expander("Conversation", expanded=True):
         download_str.append(st.session_state["past"][i])
         download_str.append(st.session_state["generated"][i])
     
-    # Can throw error - requires fix
     download_str = '\n'.join(download_str)
     if download_str:
         st.download_button('Download',download_str)
 
-# Add a button to start a new chat
   
 
 # st.button("New Chat", on_click = new_chat, type='primary')
-# Display stored conversation sessions in the sidebar
+
 # for i, sublist in enumerate(st.session_state.stored_session):
 #         with st.sidebar.expander(label= f"Conversation-Session:{i}"):
 #             st.write(sublist)
 
-# # Allow the user to clear all stored conversation sessions
 # if st.session_state.stored_session:   
 #     if st.sidebar.checkbox("Clear-all"):
 #         del st.session_state.stored_session

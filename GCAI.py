e7f5d79c0727c8e17d044d7d65e0fceef2462672
ashkaneshghi <116697979+ashkaneshghi@users.noreply.github.com>
2023-04-03 09:03:13 +0100

    Update GCAI.py

--- GCAI.py
+++ GCAI.py
@@ -94,80 +94,80 @@ API_O = st.sidebar.text_input("API-KEY", type="password")
 if API_O:
     
     def res(prompt,AIAPI):
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, max_tokens=512))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
-    index_summaries = ['Fintech related papers' for i in range(0,11)]
-    index_summaries[0] = 'WITS-WBS Gillmore Centre Fintech Workshop & Industry Conference'
-    ind = {}
-    ind[0] = GPTSimpleVectorIndex.load_from_disk('WITS_Gillmore.json', service_context=service_context)
-    for i in range(1,11):
-        ind[i] = GPTSimpleVectorIndex.load_from_disk(f'IndPapersB{i}.json', service_context=service_context)
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, max_tokens=512, openai_api_key=AIAPI))
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+        index_summaries = ['Fintech related papers' for i in range(0,11)]
+        index_summaries[0] = 'WITS-WBS Gillmore Centre Fintech Workshop & Industry Conference'
+        ind = {}
+        ind[0] = GPTSimpleVectorIndex.load_from_disk('WITS_Gillmore.json', service_context=service_context)
+        for i in range(1,11):
+            ind[i] = GPTSimpleVectorIndex.load_from_disk(f'IndPapersB{i}.json', service_context=service_context)
 
-    graph = ComposableGraph.from_indices(
-        GPTListIndex, 
-        [ind[i] for i in range(0,11)], 
-        index_summaries=index_summaries,
-        service_context=service_context)
+        graph = ComposableGraph.from_indices(
+            GPTListIndex, 
+            [ind[i] for i in range(0,11)], 
+            index_summaries=index_summaries,
+            service_context=service_context)
 
-    decompose_transform = DecomposeQueryTransform(
-        llm_predictor, verbose=True)
+        decompose_transform = DecomposeQueryTransform(
+            llm_predictor, verbose=True)
 
-    query_configs = [
-        {
-            "index_struct_type": "simple_dict",
-            "query_mode": "default",
-            "query_kwargs": {
-                "similarity_top_k": 1,
-                # "include_summary": True
+        query_configs = [
+            {
+                "index_struct_type": "simple_dict",
+                "query_mode": "default",
+                "query_kwargs": {
+                    "similarity_top_k": 1,
+                    # "include_summary": True
+                },
+                "query_transform": decompose_transform
             },
-            "query_transform": decompose_transform
-        },
-        {
-            "index_struct_type": "list",
-            "query_mode": "default",
-            "query_kwargs": {
-                "response_mode": "tree_summarize",
-                "verbose": True
-            }
-        },
-    ]
+            {
+                "index_struct_type": "list",
+                "query_mode": "default",
+                "query_kwargs": {
+                    "response_mode": "tree_summarize",
+                    "verbose": True
+                }
+            },
+        ]
 
-    index_configs = [IndexToolConfig(
-         index=ind[0], 
-         name="Vector Index 1",
-         description="useful for when you want to answer queries about Gillmore Centre Fintech Workshop & Industry Conference",
-         index_query_kwargs={"similarity_top_k": 3},
-         tool_kwargs={"return_direct": True})]
-    for i in range(1, 11):
-        tool_config = IndexToolConfig(
-            index=ind[i], 
-            name=f"Vector Index {i+1}",
-            description="useful for when you want to answer queries about fintech literature",
-            index_query_kwargs={"similarity_top_k": 3},
+        index_configs = [IndexToolConfig(
+             index=ind[0], 
+             name="Vector Index 1",
+             description="useful for when you want to answer queries about Gillmore Centre Fintech Workshop & Industry Conference",
+             index_query_kwargs={"similarity_top_k": 3},
+             tool_kwargs={"return_direct": True})]
+        for i in range(1, 11):
+            tool_config = IndexToolConfig(
+                index=ind[i], 
+                name=f"Vector Index {i+1}",
+                description="useful for when you want to answer queries about fintech literature",
+                index_query_kwargs={"similarity_top_k": 3},
+                tool_kwargs={"return_direct": True})
+            index_configs.append(tool_config)
+
+        graph_config = GraphToolConfig(
+            graph=graph,
+            name="Graph Index",
+            description="useful for when you want to answer queries about anything.",
+            query_configs=query_configs,
             tool_kwargs={"return_direct": True})
-        index_configs.append(tool_config)
 
-    graph_config = GraphToolConfig(
-        graph=graph,
-        name="Graph Index",
-        description="useful for when you want to answer queries about anything.",
-        query_configs=query_configs,
-        tool_kwargs={"return_direct": True})
+        toolkit = LlamaToolkit(
+            index_configs=index_configs,
+            graph_configs=[graph_config])
 
-    toolkit = LlamaToolkit(
-        index_configs=index_configs,
-        graph_configs=[graph_config])
+        memory = ConversationBufferMemory(memory_key="chat_history")
+        llm=OpenAI(temperature=0.1, openai_api_key=AIAPI)
+        agent_chain = create_llama_chat_agent(
+            toolkit,
+            llm,
+            memory=memory,
+            verbose=True)
+        response = agent_chain.run(input=prompt)
 
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    llm=OpenAI(temperature=0.1, openai_api_key=AIAPI)
-    agent_chain = create_llama_chat_agent(
-        toolkit,
-        llm,
-        memory=memory,
-        verbose=True)
-    response = agent_chain.run(input=prompt)
-    
-    return str(response)
+        return str(response)
 
     # os.environ['OPENAI_API_KEY'] = API_O
     def generate_response(prompt):

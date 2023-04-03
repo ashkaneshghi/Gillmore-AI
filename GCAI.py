da322001d7a895ff78740522c423bc3017f16c83
ashkaneshghi <116697979+ashkaneshghi@users.noreply.github.com>
2023-04-03 09:00:33 +0100

    Update GCAI.py

--- GCAI.py
+++ GCAI.py
@@ -29,8 +29,71 @@ if "input" not in st.session_state:
     st.session_state["input"] = ""
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
+
+# Define function to get user input
+def get_text():
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                            placeholder="Please type your question here ...", 
+                            label_visibility='hidden')
+    return input_text
+
+# Define function to start a new chat
+def new_chat():
+    save = []
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])        
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    # st.session_state.entity_memory.store = {}
+    # st.session_state.entity_memory.buffer.clear()
+
+# Set up sidebar with various options
+# with st.sidebar.expander("🛠️ ", expanded=False):
+#     # Option to preview memory store
+#     if st.checkbox("Preview memory store"):
+#         with st.expander("Memory-Store", expanded=False):
+#             st.session_state.entity_memory.store
+#     # Option to preview memory buffer
+#     if st.checkbox("Preview memory buffer"):
+#         with st.expander("Bufffer-Store", expanded=False):
+#             st.session_state.entity_memory.buffer
+#     #MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
+#     K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+
+# Set up the Streamlit app layout
+st.title(" Gillmore AI ")
+# st.subheader(" Presented by Gillmore Centre for Financial Technology")
+# st.subheader(" Powered by Llama_Index + LangChain + OpenAI + Streamlit")
+
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input("API-KEY", type="password")
+
+# Session state storage would be ideal
+#if API_O:
+    # Create an OpenAI instance
+    # llm = OpenAI(temperature=0,
+    #             openai_api_key=API_O, 
+    #             model_name=MODEL, 
+    #             verbose=False) 
+
+
+    # Create a ConversationEntityMemory object if not already created
+    # if 'entity_memory' not in st.session_state:
+    #         st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
+        
+        # Create the ConversationChain object with the specified configuration
+    # Conversation = ConversationChain(
+    #         llm=llm, 
+    #         prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+    #         memory=st.session_state.entity_memory
+    #     )  
     
-def res(prompt,AIAPI):
+if API_O:
+    
+    def res(prompt,AIAPI):
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, max_tokens=512))
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
     index_summaries = ['Fintech related papers' for i in range(0,11)]
@@ -106,69 +169,6 @@ def res(prompt,AIAPI):
     
     return str(response)
 
-
-# Define function to get user input
-def get_text():
-    input_text = st.text_input("You: ", st.session_state["input"], key="input",
-                            placeholder="Please type your question here ...", 
-                            label_visibility='hidden')
-    return input_text
-
-# Define function to start a new chat
-def new_chat():
-    save = []
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        save.append("User:" + st.session_state["past"][i])
-        save.append("Bot:" + st.session_state["generated"][i])        
-    st.session_state["stored_session"].append(save)
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["input"] = ""
-    # st.session_state.entity_memory.store = {}
-    # st.session_state.entity_memory.buffer.clear()
-
-# Set up sidebar with various options
-# with st.sidebar.expander("🛠️ ", expanded=False):
-#     # Option to preview memory store
-#     if st.checkbox("Preview memory store"):
-#         with st.expander("Memory-Store", expanded=False):
-#             st.session_state.entity_memory.store
-#     # Option to preview memory buffer
-#     if st.checkbox("Preview memory buffer"):
-#         with st.expander("Bufffer-Store", expanded=False):
-#             st.session_state.entity_memory.buffer
-#     #MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
-#     K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
-
-# Set up the Streamlit app layout
-st.title(" Gillmore AI ")
-# st.subheader(" Presented by Gillmore Centre for Financial Technology")
-# st.subheader(" Powered by Llama_Index + LangChain + OpenAI + Streamlit")
-
-# Ask the user to enter their OpenAI API key
-API_O = st.sidebar.text_input("API-KEY", type="password")
-
-# Session state storage would be ideal
-#if API_O:
-    # Create an OpenAI instance
-    # llm = OpenAI(temperature=0,
-    #             openai_api_key=API_O, 
-    #             model_name=MODEL, 
-    #             verbose=False) 
-
-
-    # Create a ConversationEntityMemory object if not already created
-    # if 'entity_memory' not in st.session_state:
-    #         st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
-        
-        # Create the ConversationChain object with the specified configuration
-    # Conversation = ConversationChain(
-    #         llm=llm, 
-    #         prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-    #         memory=st.session_state.entity_memory
-    #     )  
-    
-if API_O:
     # os.environ['OPENAI_API_KEY'] = API_O
     def generate_response(prompt):
         message = res(prompt,API_O)
@@ -179,7 +179,7 @@ else:
 
 # Get the user input
 user_input = get_text()
-st.session_state.past.append(user_input)
+# st.session_state.past.append(user_input)
 
 # Generate the output using the ConversationChain object and the user input, and add the input/output to the session
 # if user_input:
@@ -189,7 +189,7 @@ st.session_state.past.append(user_input)
 
 def send(input):
     output = generate_response(input) 
-    # st.session_state.past.append(input)  
+    st.session_state.past.append(input)  
     st.session_state.generated.append(output)  
 
 st.button("Send", on_click = send(user_input), type='primary')    
